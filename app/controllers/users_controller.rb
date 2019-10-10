f70683326467f2fbdd43fb1ce0dc021a1f3cfe5d
yoma <ym.atonement.oy999@gmail.com>
2019-10-10 20:32:19 +0900

    add form error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.find(params[:id])
+		@user = User.new(user_params)
+		if @user.save
+
+		else
+			render 'new'
+		end
 	end
+
+	private
+		
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
 
 end
